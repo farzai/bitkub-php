4e9c32db3c384c7f35c1461e5e6e7ce0f661534f
parsilver <magician_canobi@hotmail.com>
2023-12-02 21:33:06 +0700

    Update

--- src/Responses/ResponseWithValidateErrorCode.php
+++ src/Responses/ResponseWithValidateErrorCode.php
@@ -10,20 +10,18 @@ class ResponseWithValidateErrorCode extends AbstractResponseDecorator
     /**
      * Throw an exception if the response is not successfull.
      *
-     * @param  callable<\Farzai\Transport\Contracts\ResponseInterface>  $callback Custom callback to throw an exception.
+     * @param  callable|null  $callback
      *
      * @throws \Psr\Http\Client\ClientExceptionInterface
      */
     public function throw(callable $callback = null)
     {
-        $callback = $callback ?? function (ResponseInterface $response, ?\Exception $e) use ($callback) {
+        return parent::throw($callback ?? function (ResponseInterface $response, ?\Exception $e) use ($callback) {
             if ($this->json('error') !== 0) {
                 throw new \Exception(ErrorCodes::getDescription($this->json('error')));
             }
 
             return $callback ? $callback($response, $e) : $response;
-        };
-
-        return parent::throw($callback);
+        });
     }
 }
